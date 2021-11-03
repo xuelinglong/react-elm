a07c40a6ae8142f81485b5d15ec36aa0ddea5ca1
xuelinglong <921223971@qq.com>
2021-11-03 17:53:52 +0800

    11.3

--- src/views/search/Search.jsx
+++ src/views/search/Search.jsx
@@ -32,9 +32,28 @@ class Search extends Component {
       <div style={{width: this.state.width, height: this.state.height}}>
         <Loading isLoading={this.state.isLoading}></Loading>
         <PublicHeader city={this.props.guessCity} />
-        <div className="Search">
-
-        </div>
+          <div className="Search bg-white flex justify-between align-center">
+            <div className="basis-xl flex margin-right-xs">
+              <input type="text" placeholder="请输入商家或美食名城" confirm-type="search"></input>
+            </div>
+            <div className="basis-xs flex">
+              <button class="bg-blue">搜索</button>
+            </div>
+          </div>
+          <div className="content">
+            <div className="text-lg fontWeight-lg text-left padding-sm">搜索历史</div>
+            <div className="bg-white">
+              <div className="text-lg text-left padding-sm flex align-center justify-between solid-bottom">
+                <div>炸鸡</div>
+                <div className="text-gray">删除</div>
+              </div>
+              <div className="text-lg text-left padding-sm flex align-center justify-between solid-bottom">
+                <div>奶茶</div>
+                <div className="text-gray">删除</div>
+              </div>
+              <div className="text-lg fontWeight-lg text-blue padding-tb">清空搜索历史</div>
+            </div>
+          </div>
         <PublicFooter />
       </div>
     )
